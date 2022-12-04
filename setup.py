d944117c6ae7fc6c16a0c739e8077624c7106a82
Atousa Ahmadi <atousaahmadi@Atousas-MacBook-Air.local>
2022-12-04 22:25:13 +0100

    setup is modified

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = 'snowflake' ,
+    description='none',
+    version = '0.0.1',
+    author= 'atousaahmadi',
+    author_email= 'atoosaahmadi01@gmail.com',
+    license= 'Apache 2.0',
+    packages = find_packages(),
+
+
+)
